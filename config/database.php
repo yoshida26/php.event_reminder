e59dae543ab7f7c019c1428445b259bfc1efbc42
ezuremasaya <m-edzure@cmind-co.jp>
2021-03-22 08:01:03 +0900

    ブランチを変更し、内容を修正

--- config/database.php
+++ config/database.php
@@ -71,6 +71,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
