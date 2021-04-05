938634cf726024bd2375e4f40ab7b2c0607638f0
ezuremasaya <m-edzure@cmind-co.jp>
2021-04-06 00:11:01 +0900

    Slackへの通知を実装

---
+++ app/Console/Commands/SendRemindersCommandToSlack.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Event;
+use App\Notifications\EventReminderNotificationToSlack;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Notification;
+
+class SendRemindersCommandToSlack extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reminders:slack';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends reminders to Slack';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $count = 0;
+        $times = [
+            now('Europe/London')->subDay(),
+            now('Europe/London')->addDay()
+        ];
+
+        $events = Event::with('registrants')
+            ->whereHas('registrants')
+            ->whereBetween('start_time', $times)
+            ->get();
+
+        foreach ($events as $event) {
+            if (
+                // now($event->timezone)->diffInMinutes($event->start_time, false) > 0 &&
+                now($event->timezone)->diffInMinutes($event->start_time, false) <= 60
+            ) {
+                Notification::send($event->registrants, new EventReminderNotificationToSlack($event));
+                $count++;
+            }
+        }
+
+        $this->info("Event reminders of " . $count . " events has been successfully!");
+    }
+}
--- app/User.php
+++ app/User.php
@@ -44,25 +44,21 @@ class User extends Authenticatable
     protected function serializeDate(DateTimeInterface $date)
     {
         return $date->format('Y-m-d H:i:s');
-
     }
 
     public function getIsAdminAttribute()
     {
         return $this->roles()->where('id', 1)->exists();
-
     }
 
     public function getEmailVerifiedAtAttribute($value)
     {
         return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format(config('panel.date_format') . ' ' . config('panel.time_format')) : null;
-
     }
 
     public function setEmailVerifiedAtAttribute($value)
     {
         $this->attributes['email_verified_at'] = $value ? Carbon::createFromFormat(config('panel.date_format') . ' ' . config('panel.time_format'), $value)->format('Y-m-d H:i:s') : null;
-
     }
 
     public function setPasswordAttribute($input)
@@ -70,19 +66,27 @@ class User extends Authenticatable
         if ($input) {
             $this->attributes['password'] = app('hash')->needsRehash($input) ? Hash::make($input) : $input;
         }
-
     }
 
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new ResetPassword($token));
-
     }
 
     public function roles()
     {
         return $this->belongsToMany(Role::class);
-
     }
 
+    /**
+     * Route notification for Slack channel.
+     * 
+     * @param \Illuminate\Notifications\Notification $notification
+     * @return string
+     */
+
+    public function routeNotificationForSlack($notification)
+    {
+        return 'https://hooks.slack.com/services/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';
+    }
 }
